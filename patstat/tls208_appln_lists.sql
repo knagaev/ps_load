89de716dce018fc8f0d1cb0f02f8972698b263d3
knagaev <knagaev@gmail.com>
2017-05-04 18:32:15 +0300

    patstat

--- patstat/tls208_appln_lists.sql
+++ patstat/tls208_appln_lists.sql
@@ -262,3 +262,218 @@ return (select top 1 pat_publn_id from tls211_pat_publn where appln_id=@appln_id
 end
  
 GO
+
+
+
+USE [patstat2016b]
+GO
+/****** Object:  UserDefinedFunction [dbo].[base_pat_publn_id]    Script Date: 03.05.2017 17:53:19 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+create function [dbo].[base_pat_publn_id](@appln_id int) returns int as begin
+
+declare @appln_auth char(2) set @appln_auth=(select appln_auth from tls201_appln where appln_id=@appln_id)
+
+
+return
+
+case when @appln_auth in ('RU','SU') then
+
+ (select top 1 pat_publn_id from tls211_pat_publn where appln_id=@appln_id 
+  
+  
+  and publn_date<>'9999-12-31'  ---- ўЄюс√ шёъы■ўшЄ№ "ЇшъЄштэ√х чряшёш" ё яєёЄющ (NOT NULL яю єьюыўрэш■ 9999-12-31) фрЄющ т ёыєўрх, хёыш хёЄ№ эхЇшъЄштэ√х
+  
+  
+  order by publn_date desc)
+
+
+  else (select earliest_pat_publn_id from  tls201_appln where appln_id=@appln_id) end
+end
+
+
+
+
+
+select FIRST_VALUE(Name) OVER (PARTITION BY appln_id ORDER BY ListPrice ASC ) AS LeastExpensive
+
+SELECT t201.appln_id, t211.publn_auth
+FROM tls211_pat_publn t211
+  INNER JOIN tls201_appln t201
+    ON t211.appln_id = t201.appln_id 
+WHERE t211.publn_date <> '9999-12-31'
+AND t201.appln_auth in ('RU','SU')
+union all
+SELECT t201.appln_id, t211.publn_auth 
+FROM tls211_pat_publn t211 
+  INNER JOIN tls201_appln t201
+    ON t211.pat_publn_id = t201.earliest_pat_publn_id 
+AND t201.appln_auth not in ('RU','SU')
+
+SELECT t201.appln_id, 
+  CASE 
+    WHEN t201.appln_auth in ('RU','SU') THEN t211rusu.publn_auth
+    ELSE t211other.publn_auth
+  end AS base_publn_auth
+FROM tls201_appln t201
+  LEFT JOIN (SELECT t211.appln_id, t211.publn_auth 
+              FROM 
+              (SELECT appln_id, publn_auth, ROW_NUMBER() OVER (PARTITION BY appln_id ORDER BY publn_date DESC) rn 
+                FROM tls211_pat_publn WHERE publn_date <> '9999-12-31') t211
+              WHERE t211.rn = 1) t211rusu ON t211rusu.appln_id = t201.appln_id AND t201.appln_auth in ('RU','SU')
+  LEFT JOIN tls211_pat_publn t211other ON t211other.pat_publn_id = t201.earliest_pat_publn_id AND t201.appln_auth not in ('RU','SU')
+GROUP BY t201.appln_id
+
+SELECT appln_id, publn_auth 
+FROM 
+(SELECT appln_id, publn_auth, ROW_NUMBER() OVER (PARTITION BY appln_id ORDER BY publn_date DESC) rn 
+  FROM tls211_pat_publn WHERE publn_date <> '9999-12-31') t211
+WHERE rn = 1
+
+GO
+/****** Object:  UserDefinedFunction [dbo].[base_publn_auth]    Script Date: 03.05.2017 17:53:19 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+create function [dbo].[base_publn_auth](@appln_id int) returns char(2) as begin
+
+declare @appln_auth char(2) set @appln_auth=(select appln_auth from tls201_appln where appln_id=@appln_id)
+
+
+return
+
+case when @appln_auth in ('RU','SU') then
+
+ (select top 1 publn_auth from tls211_pat_publn where appln_id=@appln_id 
+  
+  
+  and publn_date<>'9999-12-31'  ---- ўЄюс√ шёъы■ўшЄ№ "ЇшъЄштэ√х чряшёш" ё яєёЄющ (NOT NULL яю єьюыўрэш■ 9999-12-31) фрЄющ т ёыєўрх, хёыш хёЄ№ эхЇшъЄштэ√х
+  
+  
+  order by publn_date desc)
+
+
+  else (select publn_auth from tls211_pat_publn where pat_publn_id=(select earliest_pat_publn_id from tls201_appln where appln_id=@appln_id)) end
+end
+
+
+
+
+
+GO
+/****** Object:  UserDefinedFunction [dbo].[base_publn_date]    Script Date: 03.05.2017 17:53:19 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+create function [dbo].[base_publn_date](@appln_id int) returns date as begin
+
+declare @appln_auth char(2) set @appln_auth=(select appln_auth from tls201_appln where appln_id=@appln_id)
+
+
+return
+
+case when @appln_auth in ('RU','SU') then
+
+ (select top 1 publn_date from tls211_pat_publn where appln_id=@appln_id 
+  
+  
+  and publn_date<>'9999-12-31'  ---- ўЄюс√ шёъы■ўшЄ№ "ЇшъЄштэ√х чряшёш" ё яєёЄющ (NOT NULL яю єьюыўрэш■ 9999-12-31) фрЄющ т ёыєўрх, хёыш хёЄ№ эхЇшъЄштэ√х
+  
+  
+  order by publn_date desc)
+
+
+  else (select earliest_publn_date from  tls201_appln where appln_id=@appln_id) end
+end
+
+
+
+
+
+GO
+/****** Object:  UserDefinedFunction [dbo].[base_publn_kind]    Script Date: 03.05.2017 17:53:19 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE function [dbo].[base_publn_kind](@appln_id int) returns char(2) as begin
+
+declare @appln_auth char(2) set @appln_auth=(select appln_auth from tls201_appln where appln_id=@appln_id)
+
+
+return
+
+case when @appln_auth in ('RU','SU') then
+
+ (select top 1 publn_kind from tls211_pat_publn where appln_id=@appln_id 
+  
+  
+  and publn_date<>'9999-12-31'  ---- ўЄюс√ шёъы■ўшЄ№ "ЇшъЄштэ√х чряшёш" ё яєёЄющ (NOT NULL яю єьюыўрэш■ 9999-12-31) фрЄющ т ёыєўрх, хёыш хёЄ№ эхЇшъЄштэ√х
+  
+  
+  order by publn_date desc)
+
+
+  else (select publn_kind from tls211_pat_publn where pat_publn_id=(select earliest_pat_publn_id from tls201_appln where appln_id=@appln_id)) end
+end
+
+
+
+
+
+GO
+/****** Object:  UserDefinedFunction [dbo].[base_publn_nr]    Script Date: 03.05.2017 17:53:19 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+CREATE function [dbo].[base_publn_nr](@appln_id int) returns varchar(15) as begin
+
+declare @appln_auth char(2) set @appln_auth=(select appln_auth from tls201_appln where appln_id=@appln_id)
+
+
+return
+
+case when @appln_auth in ('RU','SU') then
+
+ (select top 1 publn_nr from tls211_pat_publn where appln_id=@appln_id 
+  
+  
+  and publn_date<>'9999-12-31'  ---- ўЄюс√ шёъы■ўшЄ№ "ЇшъЄштэ√х чряшёш" ё яєёЄющ (NOT NULL яю єьюыўрэш■ 9999-12-31) фрЄющ т ёыєўрх, хёыш хёЄ№ эхЇшъЄштэ√х
+  
+  
+  order by publn_date desc)
+
+
+  else (select publn_nr from tls211_pat_publn where pat_publn_id=(select earliest_pat_publn_id from tls201_appln where appln_id=@appln_id)) end
+end
+
+
+
+
+
+GO
